e5ff3ea58d771872a5a08218984db9417239e0c2
Bobak Pezeshki <40700891+bbpezeshki@users.noreply.github.com>
2022-09-30 21:08:34 -0700

    update to change file names and dir names as well

--- textReplace.sh
+++ textReplace.sh
@@ -8,6 +8,10 @@ export SUFFIX=$5
 
 echo 'replacing:  '$1
 echo '     with:  '$2
-echo '       in:  -r '$3'/'$4$5
+echo '    under:  '$3'/'
+echo '     dirs:  all'
+echo '    files:  '$4$5
 
 find "$3" -name "$4$5" -exec sed -i -e "s/$TO_REPLACE/$REPLACE_TO/g" {} \;
+find "$3" -depth -type f -name "$4$5" -execdir rename "s/$TO_REPLACE/$REPLACE_TO/g" {} \;
+find "$3" -depth -type d -name "$4" -execdir rename "s/$TO_REPLACE/$REPLACE_TO/g" {} \;
